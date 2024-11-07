4af8bc8aba5cc315feca58563ce2ee3ffc6f3499
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-11-07 10:56:39 -0500

    Update IDP_Report.py

--- pages/IDP_Report.py
+++ pages/IDP_Report.py
@@ -593,7 +593,11 @@ elif primary_position == 'CM':
         defending = pd.concat([defending, ls_defending], ignore_index=True)
         defending = defending.T
         playmaking = pd.concat([playmaking, ls_playmaking], ignore_index=True)
-        playmaking = playmaking.T
+        playmaking = playmaking.melt(
+            id_vars=['Player Name', 'Team Name', 'Year'],
+            var_name='Metric',
+            value_name='Value'
+        )
     inn_columns = st.columns(4)
     with inn_columns[0]:
         st.table(passing)
