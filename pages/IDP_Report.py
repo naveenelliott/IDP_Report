c9c05f0c52928fd576d120f6061c01bb78161ed5
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-11-07 10:58:48 -0500

    Update IDP_Report.py

--- pages/IDP_Report.py
+++ pages/IDP_Report.py
@@ -593,11 +593,8 @@ elif primary_position == 'CM':
         defending = pd.concat([defending, ls_defending], ignore_index=True)
         defending = defending.T
         playmaking = pd.concat([playmaking, ls_playmaking], ignore_index=True)
-        playmaking = playmaking.pivot_table(
-            index=['Player Name', 'Year'],
-            values=['Line Break', 'Pass into Oppo Box', 'Efforts on Goal'],
-            aggfunc='sum'
-        ).reset_index()
+        st.write(playmaking)
+        playmaking = playmaking.T
     inn_columns = st.columns(4)
     with inn_columns[0]:
         st.table(passing)
