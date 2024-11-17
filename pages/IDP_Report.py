edcd813ecd529046450d61beb82e138d81455e4e
Deny461 <denis.petro6@gmail.com>
2024-11-17 17:15:58 -0500

    Update IDP_Report.py

--- pages/IDP_Report.py
+++ pages/IDP_Report.py
@@ -644,12 +644,16 @@ if primary_position == 'ATT':
     shooting = shooting.T
     inn_columns = st.columns(4)
     with inn_columns[0]:
-          # Add a title for the "Passing" table
-        st.markdown(
-        """
-        <h3 style='text-align: center; color: #6bb2e2; font-family: Arial;'>
-            PASSING
-        </h3>
+         with inn_columns[0]:
+    # Wrap the title and table in a single container with central alignment
+    st.markdown(
+        f"""
+        <div style='text-align: center; width: 100%;'>
+            <h3 style='color: #6bb2e2; font-family: Arial;'>
+                PASSING
+            </h3>
+            {passing_styled.to_html(table_attributes='style="margin: 0 auto; width: 100%;"')}
+        </div>
         """,
         unsafe_allow_html=True
         )
