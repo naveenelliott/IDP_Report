cc5ede9682936398affabcfd695d24a531ae4266
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-11-16 20:59:38 -0500

    Update IDP_Report.py

--- pages/IDP_Report.py
+++ pages/IDP_Report.py
@@ -33,6 +33,7 @@ wr_rank = getting_WeeklyReportRank()
 wr_rank = wr_rank.loc[wr_rank['Team Name'] == team_name]
 wr_rank = wr_rank.loc[wr_rank['Player Full Name'] == player_name].reset_index(drop=True)
 del wr_rank['Player Full Name'], wr_rank['Team Name']
+wr_rank = wr_rank.T
 st.write(wr_rank)
 
 directory_path = 'Player_Photos'
