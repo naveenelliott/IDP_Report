4408c01d783db3f363ffe67d3127954d32320d9f
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-11-16 19:09:27 -0500

    Update testCB_Spring.py

--- testCB_Spring.py
+++ testCB_Spring.py
@@ -55,8 +55,19 @@ def creatingPercentilesCB(merged_df):
                                         'Tackle %', 'Player Name', 'Team Name', 'Minutes'])
     player_name = merged_df.at[0, 'Player Full Name']
     team_name = merged_df.at[0, 'Team Name']
-    
-    cb_df = pd.read_csv("Thresholds/CenterBackSeasonThresholds.csv")
+    # DENIS
+    age_group = merged_df.at[0, 'Team Category']
+
+    u13_u14 = ['U13', 'U14']
+    u15_u16 = ['U15', 'U16']
+    u17_u19 = ['U17', 'U19']
+
+    if age_group in u13_u14:
+        cb_df = pd.read_csv("UpdateThresholds_IDP/CenterBackThresholds1314.csv")
+    if age_group in u15_u16:
+        cb_df = pd.read_csv("UpdateThresholds_IDP/CenterBackThresholds1516.csv")
+    if age_group in u17_u19:
+        cb_df = pd.read_csv("UpdateThresholds_IDP/CenterBackThresholds1719.csv")
     
     mean_values = cb_df.iloc[0, [5,6,7,8,9,15]]
     std_values = cb_df.iloc[1, [5,6,7,8,9,15]]
