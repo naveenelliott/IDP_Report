de32133e4d4dd592216cda206eb7061d22af9cf0
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-11-20 18:49:48 -0500

    Update PizzaPlotPDP_Spring.py

--- PizzaPlotPDP_Spring.py
+++ PizzaPlotPDP_Spring.py
@@ -85,8 +85,8 @@ def createPizzaChart(bolts):
                         slice_colors=slice_colors,       # color for individual slices
                         value_colors=text_colors,        # color for the value-text
                         value_bck_colors=slice_colors_bck,   # color for the blank spaces
-                        blank_alpha=0.4,                 # alpha for blank-space colors
-                        compare_colors = compare_colors,
+                        blank_alpha=0.4, 
+                        compare_value_colors =compare_val_colors, 
                         compare_value_bck_colors = compare_colors_bck,              # alpha for blank-space colors
                         kwargs_slices=dict(
                             edgecolor="#F2F2F2", zorder=2, linewidth=1
