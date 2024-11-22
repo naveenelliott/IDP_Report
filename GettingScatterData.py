0be3b50a738e65764863a23dd184954215061dbc
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-11-22 15:56:58 -0500

    Update GettingScatterData.py

--- GettingScatterData.py
+++ GettingScatterData.py
@@ -14,7 +14,6 @@ def gettingPostSpringGames(file_path):
         
         # Filter the list to include only CSV files
         csv_files = [file for file in files if file.endswith('.csv')]
-        st.write(csv_files)
         
         # Read each CSV file and store it in a list of DataFrames
         data_frames = []
@@ -43,6 +42,7 @@ def gettingPostSpringGames(file_path):
     
         # Optionally, combine all DataFrames into a single DataFrame
         combined_df = pd.concat(data_frames, ignore_index=True)
+        st.write(combined_df)
         combined_df = combined_df.loc[:, combined_df.columns.notna()]
         non_int = ['Player Full Name', 'Team Name', 'Position Tag']
         for col in combined_df.columns:
