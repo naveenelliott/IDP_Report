9ec9cc32a490f57c10998da14ef98384b2d90f70
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-11-22 15:22:13 -0500

    Update IDP_Report.py

--- pages/IDP_Report.py
+++ pages/IDP_Report.py
@@ -1245,6 +1245,7 @@ elif primary_position == 'CM':
             passing_styled = passing_styled.dropna(how='all', subset=['2024'])
             rename_mapping = {current: new for current, new in zip(current_names, new_names) if current in passing_styled.index}
             passing_styled = passing_styled.rename(index=rename_mapping)
+            st.write(passing_styled)
         st.write(passing_styled.to_html(table_attributes='style="width:100%"'), unsafe_allow_html=True)
     with inn_columns[1]:
         st.markdown(
