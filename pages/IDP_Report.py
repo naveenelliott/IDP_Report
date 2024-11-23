3faf32958e469a88847a4041df0702258bee4d2b
Deny461 <denis.petro6@gmail.com>
2024-11-23 14:01:48 -0500

    Update IDP_Report.py

--- pages/IDP_Report.py
+++ pages/IDP_Report.py
@@ -223,12 +223,12 @@ with info_col:
 with col2:
     st.markdown(
         """
-        <div style="text-align: right; padding-right: 20px;">
-        """,
-        unsafe_allow_html=True
+        <div style="display: flex; justify-content: center; align-items: center; height: 100%;">
+            <img src="{logo_path}" style="width: 215px; height: auto;">
+        </div>
+        """.format(logo_path=logo_path),
+        unsafe_allow_html=True,
     )
-    st.image(logo_path, width=215, use_column_width=False)
-    st.markdown("</div>", unsafe_allow_html=True)
     
 weekly_report = getting_weeklyReport()
 weekly_report = weekly_report.loc[weekly_report['Player Full Name'] == player_name]
