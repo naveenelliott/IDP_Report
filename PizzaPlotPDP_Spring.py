a8a3d383337ab75ef9c39be62e8da4b428bf15c7
Deny461 <denis.petro6@gmail.com>
2024-11-23 14:57:30 -0500

    Update PizzaPlotPDP_Spring.py

--- PizzaPlotPDP_Spring.py
+++ PizzaPlotPDP_Spring.py
@@ -23,8 +23,8 @@ def createPizzaChart(bolts):
 
     grouped = bolts.groupby('Player Name')
 
-    for player_name, group in grouped:
-    if group['Position'].iloc[0] == 'ATT':
+        for player_name, group in grouped:
+        if group['Position'].iloc[0] == 'ATT':
         player_named = group['Player Name'].iloc[0]
         cf_columns = ['Stand. Tackle Total Percentile', 'Rec Total Percentile', 'Total Passes Percentile', 'Pass Completion Percentile', 
                       'Att 1v1 Percentile', 'Loss of Poss Percentile', 'Efforts on Goal Percentile', 'Shot on Target Percentile',
