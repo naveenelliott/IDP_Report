9b03ec598fd3827130a23983647ac9d9b3af8335
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-11-24 11:42:30 -0500

    Update GettingPSDLineupData.py

--- GettingPSDLineupData.py
+++ GettingPSDLineupData.py
@@ -2,6 +2,83 @@ import pandas as pd
 import os
 import streamlit as st
 
+def getting_available_played(teamName, playerName):
+    
+    if teamName == 'Boston Bolts U13 MLS Next':
+        folder_path = 'Team_Thresholds/BoltsThirteenGames'
+        maxMins = 70
+    elif teamName == 'Boston Bolts U14 MLS Next':
+        folder_path = 'Team_Thresholds/BoltsFourteenGames'
+        maxMins = 80
+    elif teamName == 'Boston Bolts U15 MLS Next':
+        folder_path = 'Team_Thresholds/BoltsFifteenGames'
+        maxMins = 80
+    elif teamName == 'Boston Bolts U16 MLS Next':
+        folder_path = 'Team_Thresholds/BoltsSixteenGames'
+        maxMins = 90
+    elif teamName == 'Boston Bolts U17 MLS Next':
+        folder_path = 'Team_Thresholds/BoltsSeventeenGames'
+        maxMins = 90
+    elif teamName == 'Boston Bolts U19 MLS Next':
+        folder_path = 'Team_Thresholds/BoltsNineteenGames'
+        maxMins = 90
+    
+    
+    def read_all_csvs_from_folder(folder_path):
+        # List all files in the folder
+        files = os.listdir(folder_path)
+        
+        # Filter the list to include only CSV files
+        csv_files = [file for file in files if file.endswith('.csv')]
+        
+        # Read each CSV file and store it in a list of DataFrames
+        data_frames = []
+        for file in csv_files:
+            file_path = os.path.join(folder_path, file)
+            df = pd.read_csv(file_path)
+            # formatting the files
+            df.columns = df.iloc[3]
+            df = df.iloc[4:]
+            df = df.reset_index(drop=True)
+            # this is getting us the player data
+            start_index = df.index[df["Period Name"] == "Round By Player"][0]
+
+            # Find the index where "period name" is equal to "running by position player"
+            end_index = df.index[df["Period Name"] == "Round By Position Player"][0]
+
+            df = df.iloc[start_index:end_index]
+
+            # Reset the index (optional if you want a clean integer index)
+            df = df.reset_index(drop=True)
+            remove_first = ['Period Name', 'Squad Number', 'Match Name', 'As At Date', 'Round Name']
+            df = df.drop(columns=remove_first, errors='ignore')
+            df = df.dropna(axis=1, how='all')
+            df = df.iloc[1:]
+            # selecting match date information, because that's what actions have
+            details = df.loc[:, ['Player Full Name', 'Team Name', 'mins played', 'Match Date', 'Opposition', 'Starts', 'Goal', 'Assist']]
+            details[['mins played', 'Starts', 'Goal', 'Assist']] = details[['mins played', 'Starts', 'Goal', 'Assist']].astype(float)
+            details['Max Minutes'] = maxMins
+            data_frames.append(details)
+        
+        # Optionally, combine all DataFrames into a single DataFrame
+        combined_df = pd.concat(data_frames, ignore_index=True)
+        
+        return combined_df
+    
+    end = read_all_csvs_from_folder(folder_path)
+
+    end['Match Date'] = pd.to_datetime(end['Match Date'])
+    
+    # Filter rows where 'Match Date' is after 08/01/2024
+    end = end[end['Match Date'] > pd.Timestamp('2024-08-01')]
+    
+    
+    end = end.loc[end['Player Full Name'] == playerName].reset_index(drop=True)
+        
+    end = end.drop_duplicates()
+
+    return end
+
 def getting_PSD_data():
     def read_all_csvs_from_folder(folder_path):
         # List all files in the folder
