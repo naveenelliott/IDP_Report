9b8c7986a98a8ecc3e19c9ac8e2512ee69ae047d
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-11-26 23:49:57 -0500

    Update IDP_Report.py

--- pages/IDP_Report.py
+++ pages/IDP_Report.py
@@ -1199,6 +1199,7 @@ elif primary_position == 'DM':
             ).format(precision=1)
         else:
             passing = passing.apply(pd.to_numeric, errors='coerce')
+            st.write(passing)
             passing_styled = passing.style.format(precision=1)
             passing_styled = pd.concat([passing_styled, wr_rank], axis=1)
             passing_styled = passing_styled.dropna(how='all', subset=['2024'])
