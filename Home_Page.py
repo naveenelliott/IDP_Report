c3a4f56543e4e18d4fdd998cd01e6c158b961c19
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-11-27 22:37:49 -0500

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -15,7 +15,7 @@ end_df = getting_PSD_data()
 
 end_df = end_df[~end_df['Team Name'].str.contains('NAL', case=False, na=False)].reset_index()
 
-gks = ['Dylan Jacobson', 'Liam Muller', 'Jack Susi', 'Drew Cosby', 'Drew Crosby', 'Milo Ketnouvong', 'Conor Downey']
+gks = ['Dylan Jacobson', 'Liam Muller', 'Jack Susi', 'Drew Cosby', 'Drew Crosby', 'Milo Ketnouvong', 'Conor Downey', 'Ben Marro', 'Jack Seaborn']
 
 end_df = end_df.loc[~end_df['Player Full Name'].isin(gks)].reset_index()
 
