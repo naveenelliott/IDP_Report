947a8dda25c1928cbd6c4ea829947ed4ccdaa10f
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-12-03 11:37:01 -0500

    Update IDP_Report.py

--- pages/IDP_Report.py
+++ pages/IDP_Report.py
@@ -227,7 +227,7 @@ six_min_run_match = six_min_run.loc[six_min_run['Name'] == player_name_lower]
 # Pull the 'Final Time' column if a match is found
 if not six_min_run_match.empty:
     six_min_run_time = six_min_run_match['6-Min Run (km)'].values[0]
-    if pd.notna(agility_test_time) and agility_test_time.upper() != "N/A":
+    if pd.notna(six_min_run_time) and six_min_run_time.upper() != "N/A":
         six_min_run_time = f"{agility_test_time}"
     else:
         six_min_run_time = "N/A"
