e14f13da166e0c30e28936203d7981fbaabc75e2
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-12-18 20:56:33 -0500

    Update testFB_Spring.py

--- testFB_Spring.py
+++ testFB_Spring.py
@@ -21,10 +21,17 @@ def creatingPercentilesFB(merged_df):
         'Loss of Poss', 'Success', 'Unsuccess']
 
 
-    merged_df['minutes per 90'] = merged_df['mins played']/90
+    per_70 = ['U13']
+    per_80 = ['U14', 'U15']
 
-    for column in per90:
-        merged_df[column] = merged_df[column] / merged_df['minutes per 90']
+    if age_group in per_70:
+        minutes_divide = 70
+    elif age_group in per_80:
+        minutes_divide = 80
+    else:
+        minutes_divide = 90
+
+    merged_df['minutes per 90'] = merged_df['mins played'] / minutes_divide
 
         
     merged_df = merged_df.drop(columns=['minutes per 90'])
