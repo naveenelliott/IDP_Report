dcc0fb08af4bf8298f8706f582d2e6f4b75809d1
naveenelliott <119144137+naveenelliott@users.noreply.github.com>
2024-12-18 21:17:32 -0500

    Update IDP_Report.py

--- pages/IDP_Report.py
+++ pages/IDP_Report.py
@@ -782,7 +782,7 @@ player_season_raw = player_season_raw.loc[player_season_raw['Year'] == '2024'].r
 
 age_groups = player_season.at[0, 'Team Category']
 
-if not player_season_later.empty:
+#if not player_season_later.empty:
 #    player_season_later.at[0, 'Team Category'] = age_groups
 #    player_season_later_raw.at[0, 'Team Category'] = age_groups
 
